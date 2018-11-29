0a4bbcb6c168e5f1b99a2fdf9c9aef7686b073e9
root <root@lamp22google-vm.us-east1-d.c.proyectodecalles.internal>
2018-11-29 14:11:51 +0000

    first commit

---
+++ resources/templates/front/top_nav.php
@@ -0,0 +1,39 @@
+ <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+                    <img src="https://storage.googleapis.com/fotosbalonpie/slcom.png" width="30" height="30" alt="">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="shop.php">Shop</a>
+                    </li>
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                          <!--
+                    <li>
+                        <a href="admin.php">Admin</a>
+                    </li>
+                          -->
+                     <li>
+                        <a href="checkout.php">Checkout</a>
+                    </li>
+                    <li>
+                        <a href="/resumep/index.html">Mario Resume</a>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
