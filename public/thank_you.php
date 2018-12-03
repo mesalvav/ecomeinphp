bb76bb430d36c18b63cf2a5d43feb1d2f7a4ed95
root <root@lamp22google-vm.us-east1-d.c.proyectodecalles.internal>
2018-12-03 01:50:01 +0000

    finish working cart start with admin course review video#57

--- public/thank_you.php
+++ public/thank_you.php
@@ -5,19 +5,23 @@
 
 <?php
 
-// echo  $_POST['amt']. "   " . $_POST['cc'];
-//    echo $_POST['txn_id'] . "   " . $_POST['st'];
-   echo $_REQUEST['txn_id'] . "   " ;
 
-if(isset($_GET['txt'])) {
+if(isset($_GET['tx'])) {
 
     $amount = $_GET['amt'];
     $currency = $_GET['cc'];
     $transaction = $_GET['tx'];
     $status = $_GET['st'];
 
+    echo  $currency. "  " . $amount . " tx " . $_GET['tx'] . " status " . $status;
+    
+    $query = query("INSERT INTO orders (order_amount, order_transaction, order_status, order_currency) VALUES('{$amount}','{$transaction}','{$status}','{$currency}')");
+
+    confirm($query);
+
+    session_destroy();
 } else {
-    //    redirect("index.php");
+        redirect("index.php");
 
 }
 
--- resources/config.php
+++ resources/config.php
@@ -10,7 +10,7 @@ defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS ."templates/front");
 
 
-defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS ."template/back");
+defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS ."templates/back");
 
 
 defined("DB_HOST") ? null : define("DB_HOST", "localhost:3306");
